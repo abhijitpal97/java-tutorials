a8f1f02367a3bc3e56afaa1ed423ed924e2f2040
abhijitpal97 <abhijitpal97@gmail.com>
2022-05-29 19:56:58 +0530

    Eureka - Naming Service added
    Practice - Naming Register added and Gateway modified for Docker deployment

--- splring-boot-microservices1/splring-boot-microservices1/src/main/java/com/example/microservice1/microservices1/SplringBootMicroservices1Application.java
+++ splring-boot-microservices1/splring-boot-microservices1/src/main/java/com/example/microservice1/microservices1/SplringBootMicroservices1Application.java
@@ -2,12 +2,21 @@ package com.example.microservice1.microservices1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.microservice1")
 public class SplringBootMicroservices1Application {
 
+	@Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SplringBootMicroservices1Application.class, args);
 	}
--- splring-boot-microservices2/splring-boot-microservices2/src/main/java/com/example/microservice2/microservices2/SplringBootMicroservices2Application.java
+++ splring-boot-microservices2/splring-boot-microservices2/src/main/java/com/example/microservice2/microservices2/SplringBootMicroservices2Application.java
@@ -2,12 +2,21 @@ package com.example.microservice2.microservices2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.microservice2")
 public class SplringBootMicroservices2Application {
 
+	@Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SplringBootMicroservices2Application.class, args);
 	}
